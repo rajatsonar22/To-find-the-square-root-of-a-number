db2223034132327caac339f2a20cc8b819fee5e1
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-12-10 13:32:21 +0530

    Add files via upload

---
+++ To find a square root of a number.py
@@ -0,0 +1,12 @@
+#To fin a square root of a number 
+import math
+def s(n):
+    print("Square of ",n," is = ",n*n)
+    print("Square root of ",n," is =",math.sqrt(n))
+    return
+n = int(input("Ener a number"))
+s(n)
+
+
+
+
